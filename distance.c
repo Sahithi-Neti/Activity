af96e64cb58f6fc11acd5e1163dbca02ce265080
Sahithi-Neti <75069065+Sahithi-Neti@users.noreply.github.com>
2021-02-09 15:54:17 +0530

    updated

--- distance.c
+++ distance.c
@@ -3,41 +3,42 @@
 
 #include <math.h>
 
-double distance(double x1,double y1,double x2,double y2)
+double distance(double x1, double y1, double x2, double y2)
 {
-    double d,dist;
-    d = ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1));
-    dist = sqrt(d);
-    return dist;
+   double d,dist;
+   d=((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1));
+   dist = sqrt(d);
+   return dist;
 }
 
-double getx()
+double getpoint()
 {
-    double X;
-    printf("Enter 'x' Co-ordinate:\t");
-    scanf("%lf",&X);
-    return X;
+  double a;
+  printf("Value:\t");
+  scanf("%lf",&a);
+  return a;
 }
 
-double gety()
+double display(double x1, double y1, double x2, double y2, double d)
 {
-    double Y;
-    printf("Enter 'y' Co-ordinate:\t");
-    scanf("%lf",&Y);
-    return Y;
+   printf("The distance between (%lf,%lf) and (%lf,%lf) is %lf.\n",x1,y1,x2,y2,d);
+   return 0;
 }
 
 int main()
 {
-    double x1,y1,x2,y2,d;
-    printf("Enter Co-ordinates of First point\n");
-    x1=getx();
-    y1=gety();
-    printf("Enter Co-ordinates of Second point\n");
-    x2=getx();
-    y2=gety();
-    d=distance(x1,y1,x2,y2);
-    printf("The Distance between (%lf,%lf) and (%lf,%lf) is %lf\n",x1,y1,x2,y2,d);
-
-    return 0;
+  double x1,y1,x2,y2,d;
+  printf("Enter the Co-ordinates of First point:\n");
+  printf(" 'x' Co-ordinate:\n");
+  x1=getpoint();
+  printf(" 'y' Co-ordinate:\n");
+  y1=getpoint();
+  printf("Enter the Co-ordinates of Second point:\n");
+  printf(" 'x' Co-ordinate:\n");
+  x2=getpoint();
+  printf(" 'y' Co-ordinate:\n");
+  y2=getpoint();
+  d=distance(x1,y1,x2,y2);
+  display(x1,y1,x2,y2,d);
+   return 0;
 }
