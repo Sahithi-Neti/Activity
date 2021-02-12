a07548c8e035ae55375177712b5866a6d81ddc7c
Sahithi-Neti <75069065+Sahithi-Neti@users.noreply.github.com>
2021-02-12 15:14:04 +0530

    updated

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -4,45 +4,44 @@
 
 struct points
 {
-    double a;
+    double x1,y1,x2,y2;
 }p;
 
 double distance(double x1,double y1,double x2,double y2)
 {
     double d,dist;
-    d = ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1));
+    d = ((p.x2-p.x1)*(p.x2-p.x1)) + ((p.y2-p.y1)*(p.y2-p.y1));
     dist = sqrt(d);
     return dist;
 }
 
 double getpoint()
 {
-    printf("Value:\t");
-    scanf("%lf",&p.a);
-    return p.a;
+    printf("Enter Co-ordinates of First point\n");
+    printf(" 'x' Co-ordinate:\n");
+    scanf("%lf",&p.x1);
+    printf(" 'y' Co-ordinate:\n");
+    scanf("%lf",&p.y1);
+    printf("Enter Co-ordinates of Second point\n");
+    printf(" 'x' Co-ordinate:\n");
+    scanf("%lf",&p.x2);
+    printf(" 'y' Co-ordinate:\n");
+    scanf("%lf",&p.y2);
+    return 0.00;
 }
 
 double display(double x1, double y1, double x2, double y2, double d)
 {
-    printf("The Distance between (%lf,%lf) and (%lf,%lf) is %lf\n",x1,y1,x2,y2,d);
+    printf("The Distance between (%lf,%lf) and (%lf,%lf) is %lf\n",p.x1,p.y1,p.x2,p.y2,d);
     return 0.00;
 }
 
 int main()
 {
-    double x1,y1,x2,y2,d;
-    printf("Enter Co-ordinates of First point\n");
-    printf(" 'x' Co-ordinate:\n");
-    x1=getpoint();
-    printf(" 'y' Co-ordinate:\n");
-    y1=getpoint();
-    printf("Enter Co-ordinates of Second point\n");
-    printf(" 'x' Co-ordinate:\n");
-    x2=getpoint();
-    printf(" 'y' Co-ordinate:\n");
-    y2=getpoint();
-    d=distance(x1,y1,x2,y2);
-    display(x1,y1,x2,y2,d);
+    double d;
+    getpoint();
+    d=distance(p.x1,p.y1,p.x2,p.y2);
+    display(p.x1,p.y1,p.x2,p.y2,d);
 
     return 0;
 }
