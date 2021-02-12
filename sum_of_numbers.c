5df0de0db603deaae348637a1f5c7f00fefd5e99
Sahithi-Neti <75069065+Sahithi-Neti@users.noreply.github.com>
2021-02-12 14:13:19 +0530

    updated

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -29,21 +29,31 @@ int compute(int n, int num[])
     return sum;
 }
 
-int display(int sum)
+int display(int n, int sum, int num[])
 {
-    printf("The sum of given numbers is %d\n",sum);
+   printf("Numbers Entered:\n");
+   for(int i=0; i<n; i++)
+   {
+      printf("%d\n",num[i]);
+    }
+    printf("Therefore, the sum is %d\n",sum);
     return 0;
 }
 
-
+int count()
+{
+   int n;
+   printf("Enter total count of numbers(n)\n");
+   scanf("%d",&n);
+   return n;
+}
 int main()
 {
     int n,sum;
     int *x;
-    printf("Enter total count of numbers(n)\n");
-    scanf("%d",&n);
+    n = count();
     x = numbers(n);
     sum = compute(n,x);
-    display(sum);
-    
-} 
+    display(n,sum,x);
+     return 0;
+}
