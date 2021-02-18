ba3f852fb1ea7809b00a3affbdd0c8d77613a3e0
Sahithi-Neti <75069065+Sahithi-Neti@users.noreply.github.com>
2021-02-18 17:11:26 +0530

    updated

--- two_fractions.c
+++ two_fractions.c
@@ -4,52 +4,54 @@
 
 struct frac{
     int n,d;
-}f,p,r;
+};
 
 
-int value()
+int value(struct frac*f, struct frac*p)
 {
     printf("First fraction\n");
     printf("Numerator-Value:\t");
-    scanf("%d",&f.n);
+    scanf("%d",&f->n);
     printf("Denominator-Value:\t ");
-    scanf("%d",&f.d);
+    scanf("%d",&f->d);
     printf("Second fraction\n");
     printf("Numerator-Value:\t");
-    scanf("%d",&p.n);
+    scanf("%d",&p->n);
     printf("Denominator-Value\t");
-    scanf("%d",&p.d);
+    scanf("%d",&p->d);
     return 0;
 }
 
-void compute()
+void compute(struct frac*f, struct frac*p, struct frac*r)
 {
     int d;
     float s1,s2,sum;
-    r.n = (f.n*p.d)+(p.n*f.d);
-    r.d = f.d*p.d;
-    for(int i=1; i<=r.n && i<=r.d; i++)
+    r->n = (f->n*p->d)+(p->n*f->d);
+    r->d = f->d*p->d;
+    for(int i=1; i<=r->n && i<=r->d; i++)
     {
-        if(r.n%i==0 && r.d%i==0)
+        if(r->n%i==0 && r->d%i==0)
         d=i;
     }
-    r.n = r.n/d;
-    r.d = r.d/d;
+    r->n = r->n/d;
+    r->d = r->d/d;
 
 }
 
 
-void display()
+void display(struct frac*f, struct frac*p, struct frac*r)
 {
-    printf("SUM:\n %d/%d + %d/%d = %d/%d\n",f.n,f.d,p.n,p.d,r.n,r.d);
+    printf("SUM:\n %d/%d + %d/%d = %d/%d\n",f->n,f->d,p->n,p->d,r->n,r->d);
 }
 
 
 int main()
 {
-    float sum;
-    value();
-    compute();
-    display();
+    struct frac f;
+    struct frac p;
+    struct frac r;
+    value(&f,&p);
+    compute(&f,&p,&r);
+    display(&f,&p,&r);
     return 0;
 }
